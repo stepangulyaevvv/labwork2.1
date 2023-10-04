a67d066e3d5e2cb4e185ebfde506810935ac9060
gulya <gulya@STEPAN>
2023-10-04 12:22:45 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	const float PI = 3.14159265358979323846;
+	cout << "Введите длину диаметра окружности d: ";
+	float d;
+	cin >> d;
+	float L = 2 * PI * (0.5 * d);
+	cout << "Длина окружности L равна: " << L;
+}
